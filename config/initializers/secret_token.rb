da3d05affb43656f2f00ca308dce647f49b144ee
inframatic <isteffy@hotmail.com>
2013-08-28 09:31:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterHartl::Application.config.secret_key_base = 'b2c3344930a9e154be9d9dde597ed1bf0d7f4adafd673b781deb6da5282aea838bde6b4f67905c95ae06fd2be9fed662555ab511e265b030be3035e8ddd45b54'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwitterHartl::Application.config.session_store :cookie_store, key: '_twitter-hartl_session'
